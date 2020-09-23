30e8bbf9b5dcef40fcbfa64bc9acc77d5b172950
Hannah Olukoye <hannah.olukoye@macro-eyes.com>
2020-09-23 13:39:46 +0300

    WIP: Set up codebase structure in mvvm

--- app/src/main/java/com/hannah/mybookingapp/authentication/AuthActivity.kt
+++ app/src/main/java/com/hannah/mybookingapp/authentication/AuthActivity.kt
@@ -8,6 +8,5 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
     }
 }
